2dd3335f875d4609a7ebdba36127ff1a31a04d86
lucafedeli88 <luca.fedeli@cea.fr>
2026-09-23 23:19:28 +0200

    correct bug

--- Source/Particles/PhysicalParticleContainer.cpp
+++ Source/Particles/PhysicalParticleContainer.cpp
@@ -276,14 +276,14 @@ PhysicalParticleContainer::PhysicalParticleContainer (AmrCore* amr_core, int isp
     pp_species_name.query("do_qed_quantum_sync", m_do_qed_quantum_sync);
     if (m_do_qed_quantum_sync) {
         AddRealComp("opticalDepthQSR");
-        AddRealComp("chi_lepton_at_creation");
-        AddRealComp("E_field_at_creation");
-        AddRealComp("B_field_at_creation");
     }
 
     pp_species_name.query("do_qed_breit_wheeler", m_do_qed_breit_wheeler);
     if (m_do_qed_breit_wheeler) {
         AddRealComp("opticalDepthBW");
+        AddRealComp("chi_lepton_at_creation");
+        AddRealComp("E_field_at_creation");
+        AddRealComp("B_field_at_creation");
     }
 
     if(m_do_qed_quantum_sync){
