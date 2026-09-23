c1b757bc059096f0daae4a177efb9542570665d4
lucafedeli88 <luca.fedeli@cea.fr>
2026-09-23 23:32:25 +0200

    fixed bug

--- Source/Particles/PhysicalParticleContainer.cpp
+++ Source/Particles/PhysicalParticleContainer.cpp
@@ -281,6 +281,9 @@ PhysicalParticleContainer::PhysicalParticleContainer (AmrCore* amr_core, int isp
     pp_species_name.query("do_qed_breit_wheeler", m_do_qed_breit_wheeler);
     if (m_do_qed_breit_wheeler) {
         AddRealComp("opticalDepthBW");
+    }
+
+    if(AmIA<PhysicalSpecies::photon>()){
         AddRealComp("chi_lepton_at_creation");
         AddRealComp("E_field_at_creation");
         AddRealComp("B_field_at_creation");
